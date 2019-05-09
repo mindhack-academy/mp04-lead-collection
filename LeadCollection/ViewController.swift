7f74327687556dbc0d2e41eaedd0cb072bc173ae
Marius Patru <marius@mindhack.academy>
2019-05-09 20:05:35 +0300

    Initial Commit

---
+++ LeadCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LeadCollection
+//
+//  Created by Trainer on 09/05/2019.
+//  Copyright © 2019 Trainer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
